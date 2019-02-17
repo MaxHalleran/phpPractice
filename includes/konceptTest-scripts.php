861c543bd756d568f89870de7ac7b7ee3eed2634
MaxHalleran <maxhalleran@gmail.com>
2019-02-16 18:00:44 -0700

    Added a settings link

--- includes/konceptTest-scripts.php
+++ includes/konceptTest-scripts.php
@@ -2,11 +2,9 @@
   // Add Scripts
   function kt_add_scripts() {
     // Add main css
-    wp_enqueue_style('kt-main-style', plugins_url().'/konceptTest/css/style.css');
+    wp_enqueue_style('kt-main-style', plugins_url() . '/konceptTest/css/style.css');
     // Add main js
-    wp_enqueue_script('kt-main-script', plugins_url().'/konceptTest/js/main.js');
-    // Add test shortcode
-    // wp_enqueue_script('kt-test-shortcode', plugins_url().'/konceptTest/includes/test-shortcode.php');
+    wp_enqueue_script('kt-main-script', plugins_url() . '/konceptTest/js/main.js');
   }
 
   add_action('wp_enqueue_scripts', 'kt_add_scripts');
--- konceptTest.php
+++ konceptTest.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Koncept Test
 Plugin URI: https://maxhalleran.github.io
-Description: A simple plugin that adds a [test] shortcode that outputs 'TEST SUCCEEDED'
+Description: A simple plugin that adds a [test] shortcode that outputs 'TEST SUCCEEDED' and an [instafeed] shortcode that outputs the latest five images of a user.
 Version: 1.0.0
 Author: Max Halleran
 Author URI: https://maxhalleran.github.io
@@ -16,4 +16,25 @@ if(!defined('ABSPATH')) {
 // Load scripts
 require_once(plugin_dir_path(__FILE__).'/includes/konceptTest-scripts.php');
 
+// Add settings link
+// add_filter('plugin_action_links_'.plugin_basename(__FILE__), 'add_plugin_page_settings_link');
+// function add_plugin_page_settings_link( $links ) {
+// 	$links[] = '<a href="' .
+// 		admin_url( 'options-general.php?page=my-plugin' ) .
+// 		'">' . __('Settings') . '</a>';
+// 	return $links;
+// }
+
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'add_plugin_page_settings_link');
+
+function add_plugin_page_settings_link ( $links ) {
+ $mylinks = array(
+ '<a href="' . admin_url( 'options-general.php?page=myplugin' ) . '">Settings</a>',
+ );
+return array_merge( $links, $mylinks );
+}
+
+// Add test shortcode
 require( plugin_dir_path(__FILE__) . '/includes/test-shortcode.php');
+// Add insta shortcode
+require( plugin_dir_path(__FILE__) . '/includes/instagram-feed-shortcode.php');
